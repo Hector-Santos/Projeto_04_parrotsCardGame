7cde9df0de418ac0a50c8881968c55f8c506859b
Hector Santos <habs.404@gmail.com>
2022-04-08 18:20:19 -0300

    versão fim

--- scripts.js
+++ scripts.js
@@ -1,16 +1,22 @@
+let arraycompara = []
 function viracarta(elemento) {
-  console.log(elemento) 
-  elemento.classList.add("virado")
-  function escondefrente() {
-    elemento.querySelector(".frente").classList.add("escondido")
-  }
-  setTimeout(escondefrente, 100)
-  function mostraverso() {
-    elemento.querySelector(".verso").classList.remove("escondido")
-  }
-  setTimeout(mostraverso, 100)
+  if (arraycompara.length < 2 && elemento.classList.contains("virado") != true) {
+    elemento.classList.add("virado")
+    function escondefrente() {
+      elemento.querySelector(".frente").classList.add("escondido")
+    }
+    setTimeout(escondefrente, 100)
+    function mostraverso() {
+      elemento.querySelector(".verso").classList.remove("escondido")
+    }
+    setTimeout(mostraverso, 100)
 
+    arraycompara.push(elemento)
+    setTimeout(comparaCartas, 2000)
+  }
 }
+
+
 
 function adicionacartas() {
   let numerocartas = prompt("Com quantas cartas deseja jogar? Escolha um numero par entre 4 e 14")
@@ -19,70 +25,74 @@ function adicionacartas() {
   let array = [`<div class="carta " onclick="viracarta(this)">
       <img class="frente" src="imagens/front.png">
       <img class="verso escondido" src="imagens/explodyparrot.gif">
-    </div>` ,
-      `<div class="carta " onclick="viracarta(this)">
+    </div>`,
+    `<div class="carta" onclick="viracarta(this)">
       <img class="frente" src="imagens/front.png">
       <img class="verso escondido" src="imagens/explodyparrot.gif">
-    </div>` ,
-     `<div class="carta" onclick="viracarta(this)">
+    </div>`,
+    `<div class="carta" onclick="viracarta(this)">
       <img class="frente" src="imagens/front.png">
       <img class="verso escondido" src="imagens/fiestaparrot.gif">
     </div>`,
-      `<div class="carta" onclick="viracarta(this)">
+    `<div class="carta" onclick="viracarta(this)">
       <img class="frente" src="imagens/front.png">
       <img class="verso escondido" src="imagens/fiestaparrot.gif">
     </div>`,
-      `<div class="carta " onclick="viracarta(this)">
+    `<div class="carta" onclick="viracarta(this)">
        <img class="frente" src="imagens/front.png">
        <img class="verso escondido" src="imagens/metalparrot.gif">
      </div>`,
-      `<div class="carta " onclick="viracarta(this)">
-     <img class="frente" src="imagens/front.png">
-     <img class="verso escondido" src="imagens/metalparrot.gif">
-   </div>`,
-      `<div class="carta" onclick="viracarta(this)">
+    `<div class="carta" onclick="viracarta(this)">
+       <img class="frente" src="imagens/front.png">
+       <img class="verso escondido" src="imagens/metalparrot.gif">
+     </div>`,
+    `<div class="carta" onclick="viracarta(this)">
        <img class="frente" src="imagens/front.png">
        <img class="verso escondido" src="imagens/revertitparrot.gif">
      </div>`,
-      `<div class="carta" onclick="viracarta(this)">
-     <img class="frente" src="imagens/front.png">
-     <img class="verso escondido" src="imagens/revertitparrot.gif">
-   </div>`,
-      `<div class="carta" onclick="viracarta(this)">
-     <img class="frente" src="imagens/front.png">
-     <img class="verso escondido" src="imagens/tripletsparrot.gif">
-   </div>`,
-      `<div class="carta" onclick="viracarta(this)">
-     <img class="frente" src="imagens/front.png">
-     <img class="verso escondido" src="imagens/tripletsparrot.gif">
-   </div>`,
-   `<div class="carta" onclick="viracarta(this)">
+    `<div class="carta" onclick="viracarta(this)">
+       <img class="frente" src="imagens/front.png">
+       <img class="verso escondido" src="imagens/revertitparrot.gif">
+     </div>`,
+    `<div class="carta" onclick="viracarta(this)">
+    <img class="frente" src="imagens/front.png">
+    <img class="verso escondido" src="imagens/tripletsparrot.gif">
+    </div>`,
+    `<div class="carta" onclick="viracarta(this)">
+    <img class="frente" src="imagens/front.png">
+    <img class="verso escondido" src="imagens/tripletsparrot.gif">
+    </div>`,
+    `<div class="carta" onclick="viracarta(this)">
    <img class="frente" src="imagens/front.png">
    <img class="verso escondido" src="imagens/unicornparrot.gif">
    </div>`,
-   `<div class="carta" onclick="viracarta(this)">
+    `<div class="carta" onclick="viracarta(this)">
    <img class="frente" src="imagens/front.png">
    <img class="verso escondido" src="imagens/unicornparrot.gif">
    </div>`,
-   `<div class="carta" onclick="viracarta(this)">
+    `<div class="carta" onclick="viracarta(this)">
    <img class="frente" src="imagens/front.png">
    <img class="verso escondido" src="imagens/bobrossparrot.gif">
    </div>`,
-   `<div class="carta" onclick="viracarta(this)">
+    `<div class="carta" onclick="viracarta(this)">
    <img class="frente" src="imagens/front.png">
    <img class="verso escondido" src="imagens/bobrossparrot.gif">
-   </div>`,]
+   </div>`]
 
-  function comparador() {
+  function aleatorio() {
     return Math.random() - 0.5
   }
-  document.querySelector(".cartas").innerHTML += array.slice(0,numerocartas).sort(comparador)
+
+  array = array.slice(0, numerocartas).sort(aleatorio);
+
+  for (let i = 0; i < array.length; i++) {
+    document.querySelector(".cartas").innerHTML += array[i];
+  }
 }
 
-function viraDevolta() {
-  let elemento = document.querySelector(".virado")
-  elemento.classList.remove("virado")
+function viraDevolta(elemento) {
   elemento.classList.add("revirado")
+  elemento.classList.remove("virado")
   function escondeverso() {
     elemento.querySelector(".verso").classList.add("escondido")
   }
@@ -91,12 +101,19 @@ function viraDevolta() {
     elemento.querySelector(".frente").classList.remove("escondido")
   }
   setTimeout(mostrafrente, 100)
+  elemento.classList.remove("revirado")
 }
 
 
 
-function comparaCartas(){
-let viradas = document.querySelectorAll(".virado")
-console.log(viradas)
+function comparaCartas() {
+
+  if (arraycompara[0].innerHTML !== arraycompara[1].innerHTML) {
+    viraDevolta(arraycompara[0])
+    viraDevolta(arraycompara[1])
+  }
+  arraycompara = []
 }
+
+
 adicionacartas()
